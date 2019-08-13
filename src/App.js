a7379339011b51a4a8fcf78ddaddd4d6a16da2cc
MahaVajda <mvajda@post.sk>
2019-08-13 12:44:23 +0200

    app
    zmena

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Vypis text="daco" />
+      <Vypis text="daco dalsie" />
     </div>
   );
 };
